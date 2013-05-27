e4b06a261027b748c82658b138e319fe3bf8c926
ronaldpatino <ronaldpatino@gmail.com>
2013-05-27 17:24:50 -0500

    Terminado layout seccion titulares

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <?php wp_head(); ?>
+
+    <script src="assets/js/holder.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/scripts/holder.js"></script>
 </head>
 <body>
 
@@ -31,38 +34,252 @@
 </div>
 
 
-<div id="main" class="container">
-    <div class="row">
-        <div class="span8">
-            <h3>
-                Span 4
-            </h3>
-            <p>
-                Content
-            </p>
-        </div>
-        <div  class="span4">
-            <h3>
-                Span 4
-            </h3>
-            <p>
-                Content
-            </p>
+<div class="container">
+    <!-- Main hero unit for a primary marketing message or call to action -->
+    <div class="hero-unit">
+        <h1>logo</h1>
+    </div>
+
+    <!-- Inicio Menu -->
+    <div class="navbar">
+        <div class="navbar-inner">
+            <div class="container">
+                <ul class="nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Downloads</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
         </div>
     </div>
+    <!-- /.navbar -->
+
+    <ul class="breadcrumb">
+        <li><a href="#">Home</a> <span class="divider">/</span></li>
+        <li><a href="#">Library</a> <span class="divider">/</span></li>
+        <li class="active">Data</li>
+    </ul>
+
+    <!-- Fin Menu-->
+</div>
+
+<div id="main" class="container">
+
+
+    <!-- Inicio Titulares -->
+    <div class="row">
+        <div class="span8">
+            <!-- Inicio Titular principal-->
+            <div class="row">
+                <div class="span8">
+                    <h3>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h3>
+                    <img data-src="holder.js/624x324" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <!-- Fin Titular principal-->
+
+            <!-- Inicio Fila Titulares-->
+            <div class="row">
+                <div class="span4">
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h4>
+                    <img data-src="holder.js/305x150" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <div class="span4">
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h4>
+                    <img data-src="holder.js/305x150" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <!-- Fin Fila Titulares-->
+
+            <!-- Inicio Fila Titulares-->
+            <div class="row">
+                <div class="span4">
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h4>
+                    <img data-src="holder.js/305x150" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <div class="span4">
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h4>
+                    <img data-src="holder.js/305x150" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <!-- Fin Fila Titulares-->
+
+            <!-- Inicio Fila Titulares-->
+            <div class="row">
+                <div class="span4">
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h4>
+                    <img data-src="holder.js/305x150" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+                <div class="span4">
+                    <h4>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                    </h4>
+                    <img data-src="holder.js/305x150" class="img-polaroid">
+
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                </div>
+            </div>
+            <!-- Fin Fila Titulares-->
+        </div>
+
+
+        <!-- Ultimas noticias -->
+        <div class="span4">
+            <div class="row">
+                <div class="span4">
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                    <div>
+                        <h4>Very <strong>customizable</strong></h4>
+
+                        <p>Lorem ipsum dolor sit amet nsectetuer nec Vivamus. Curabitu laoreet amet eget. Viurab oremd
+                            ellentesque ameteget. </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <!-- Publicidad -->
+                <div class="span4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/300x200" alt="">
+                    </div>
+                </div>
+                <!-- Fin Publicidad -->
+            </div>
+
+            <div class="row">
+                <!-- Portada -->
+                <div class="span4">
+                    <div class="thumbnail">
+                        <img data-src="holder.js/300x400" alt="">
+                    </div>
+                </div>
+                <!-- Fin Portada -->
+            </div>
+        </div>
+        <!-- Fin ultimas noticias -->
+    </div>
+
+
+    <!-- Fin Titulares -->
 </div>
 
 <div class="navbar navbar-inverse navbar-fixed-bottom">
     <div class="navbar-inner">
         <div class="container">
             <a class="brand" href="#">Project name</a>
+
             <div class="nav-collapse collapse">
                 <ul class="nav">
                     <li class="active"><a href="#">Home</a></li>
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
-            </div><!--/.nav-collapse -->
+            </div>
+            <!--/.nav-collapse -->
         </div>
     </div>
 </div>
