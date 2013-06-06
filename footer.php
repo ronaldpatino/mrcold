683fc97b87d7b50e1da91fef8826c50c0799060e
ronaldpatino <ronaldpatino@gmail.com>
2013-06-06 13:36:23 -0500

    Movidos contenidos a footer

--- footer.php
+++ footer.php
@@ -1,14 +1,27 @@
-<?php get_sidebar(); ?>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/scripts/holder.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function($) {
 
-<footer role="contentinfo">
-	<p>&copy;<?php echo date("Y"); ?> <a href="#top" title="Jump back to top">&#8593;</a></p>
-</footer>
+        $('[data-toggle="modal"]').click(function(e) {
+            e.preventDefault();
+            var imagen_sociales = $(this).data('img');
+            var caption_sociales = $(this).data('caption');
 
-<?php wp_footer(); ?>
+            $(".modal-body #imagen_sociales").attr('src', imagen_sociales);
+            $(".modal-footer").html('<p>'+ caption_sociales +'</p>');
+        });
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script>!window.jQuery && document.write(unescape('%3Cscript src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/jquery-1.9.0.js"%3E%3C/script%3E'))</script>
-<script src="<?php echo bloginfo('template_directory'); ?>/assets/scripts/general.js"></script>
-<?php if ( is_singular() ) wp_print_scripts( 'comment-reply' ); ?>
-</body>
-</html>
+        $('[data-toggle="modal"]').click(function(e) {
+            e.preventDefault();
+            var imagen_multimedia = $(this).data('img');
+            var caption_multimedia = $(this).data('caption');
+
+            $(".modal-body #imagen_multimedia").attr('src', imagen_multimedia);
+            $(".modal-footer").html('<p>'+ caption_multimedia +'</p>');
+        });
+    });
+</script>
+
+
+<?php get_template_part( 'blocks/modalsociales'); ?>
+<?php get_template_part( 'blocks/modalmultimedia'); ?>
--- index.php
+++ index.php
@@ -68,34 +68,7 @@
 
 
 <?php wp_footer(); ?>
-
-<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/scripts/holder.js"></script>
-<script type="text/javascript">
-    jQuery(document).ready(function($) {
-
-        $('[data-toggle="modal"]').click(function(e) {
-            e.preventDefault();
-            var imagen_sociales = $(this).data('img');
-            var caption_sociales = $(this).data('caption');
-
-            $(".modal-body #imagen_sociales").attr('src', imagen_sociales);
-            $(".modal-footer").html('<p>'+ caption_sociales +'</p>');
-        });
-
-        $('[data-toggle="modal"]').click(function(e) {
-            e.preventDefault();
-            var imagen_multimedia = $(this).data('img');
-            var caption_multimedia = $(this).data('caption');
-
-            $(".modal-body #imagen_multimedia").attr('src', imagen_multimedia);
-            $(".modal-footer").html('<p>'+ caption_multimedia +'</p>');
-        });
-    });
-</script>
-
-
-<?php get_template_part( 'blocks/modalsociales'); ?>
-<?php get_template_part( 'blocks/modalmultimedia'); ?>
+<?php get_footer(); ?>
 
 </body>
 </html>
