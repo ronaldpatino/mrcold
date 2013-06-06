52f4b023e3cc7905e221e523add1a78a9f23493d
ronaldpatino <ronaldpatino@gmail.com>
2013-06-06 13:01:41 -0500

    Agregado twitter

--- index.php
+++ index.php
@@ -1006,7 +1006,20 @@
 </div>
 <!-- Fin menu-pie-->
 
-<!-- Fin menu pie -->
+<div class="navbar-custom navbar-inverse navbar-fixed-bottom">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="brand" href="#"><img title="El Mercurio Noticias Ecuador Twitter"  alt="El Mercurio Noticias Ecuador Twitter" class="pull-right img-top" src="<?php bloginfo('template_url'); ?>/assets/img/twitter.png"></a>
+            <div class="nav-collapse collapse">
+                <ul class="nav">
+                    <li ><a href="#">Vía @ecuainm: Se reinstaló audiencia contra 17 policías de radio patrulla por el 30-S http://bit.ly/13dvAtH</a></li>
+
+
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
 
 <?php wp_footer(); ?>
 
