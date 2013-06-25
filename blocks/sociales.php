d143646f2d4400237e5c2f5fea360396508c6196
ronaldpatino <ronaldpatino@gmail.com>
2013-06-25 13:18:16 -0500

    Corregido span de segundo bloque

--- blocks/sociales.php
+++ blocks/sociales.php
@@ -5,7 +5,7 @@
             <h2>Sociales</h2>
             <?php if ( dynamic_sidebar('sociales') ) : else : endif; ?>
         </div>
-        <div class="span8">
+        <div class="span4">
         </div>
     </div>
 </div>
