90a3e4ec716ed1e17dd4b378a57b15500610fef0
ronaldpatino <ronaldpatino@gmail.com>
2013-06-25 13:17:55 -0500

    reemplazado por widget

--- blocks/multimedia.php
+++ blocks/multimedia.php
@@ -2,91 +2,10 @@
 <div class='container margen-container'>
     <div id='multimedia' class='row-fluid'>
         <div class="span8">
-            <h2>
-                Multimedia
-            </h2>
-            <!-- Carrusel multimedia -->
-            <div class="carousel slide" id="carruselmultimedia">
-                <div class="carousel-inner">
-                    <div class="item active">
-                        <ul class="thumbnails multimedia-thumbnails">
-                            <li class="span4">
-                                <div class="thumbnail multimedia-thumbnails-item">
-                                    <a href="#modal_multimedia" data-caption="Thumbnail caption..."  data-img="<?php bloginfo('template_url'); ?>/assets/img/placeholder-multimedia.jpg" data-toggle="modal">
-                                        <img data-src="holder.js/230x164">
-                                    </a>
-                                    <p>Thumbnail caption...</p>
-                                </div>
-                            </li>
-
-                            <li class="span4">
-                                <div class="thumbnail multimedia-thumbnails-item">
-                                    <a href="#modal_multimedia" data-caption="Thumbnail caption..."  data-img="<?php bloginfo('template_url'); ?>/assets/img/placeholder-multimedia.jpg" data-toggle="modal">
-                                        <img data-src="holder.js/230x164">
-                                    </a>
-                                    <p>Thumbnail caption...</p>
-                                </div>
-                            </li>
-
-
-                            <li class="span4">
-                                <div class="thumbnail multimedia-thumbnails-item">
-                                    <a href="#modal_multimedia" data-caption="Thumbnail caption..."  data-img="<?php bloginfo('template_url'); ?>/assets/img/placeholder-multimedia.jpg" data-toggle="modal">
-                                        <img data-src="holder.js/230x164">
-                                    </a>
-                                    <p>Thumbnail caption...</p>
-                                </div>
-                            </li>
-
-                        </ul>
-                    </div>
-
-                    <div class="item">
-                        <ul class="thumbnails multimedia-thumbnails">
-                            <li class="span4">
-                                <div class="thumbnail multimedia-thumbnails-item">
-                                    <a href="#modal_multimedia" data-caption="Thumbnail caption..." data-img="<?php bloginfo('template_url'); ?>/assets/img/flechas.png" data-toggle="modal">
-                                        <img data-src="holder.js/230x164">
-                                    </a>
-                                    <p>Thumbnail caption...</p>
-                                </div>
-                            </li>
-
-                            <li class="span4">
-                                <div class="thumbnail multimedia-thumbnails-item">
-                                    <a href="#modal_multimedia" data-caption="Thumbnail caption..."  data-img="<?php bloginfo('template_url'); ?>/assets/img/placeholder-multimedia.jpg" data-toggle="modal">
-                                        <img data-src="holder.js/230x164">
-                                    </a>
-                                    <p>Thumbnail caption...</p>
-                                </div>
-                            </li>
-
-
-                            <li class="span4">
-                                <div class="thumbnail multimedia-thumbnails-item">
-                                    <a href="#modal_multimedia" data-caption="Thumbnail caption..."  data-img="<?php bloginfo('template_url'); ?>/assets/img/placeholder-multimedia.jpg" data-toggle="modal">
-                                        <img data-src="holder.js/230x164">
-                                    </a>
-                                    <p>Thumbnail caption...</p>
-                                </div>
-                            </li>
-
-                        </ul>
-                    </div>
-                </div>
-                <a data-slide="prev" href="#carruselmultimedia" class="left multimedia-carousel-control">&lt;</a>
-                <a data-slide="next" href="#carruselmultimedia" class="right multimedia-carousel-control">&gt;</a>
-            </div>
-            <!-- Fin Carrusel multimedia -->
+            <h2>Multimedia</h2>
+            <?php if ( dynamic_sidebar('multimedia') ) : else : endif; ?>
         </div>
         <div class="span4">
-            <ul class="thumbnails publicidad-thumbnails">
-                <li class="span12">
-                    <div class="thumbnail publicidad">
-                        <img data-src="holder.js/300x300">
-                    </div>
-                </li>
-            </ul>
         </div>
     </div>
 
