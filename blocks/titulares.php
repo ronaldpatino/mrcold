233aa038c1d1172923f8116b6e1dd233dea04618
ronaldpatino <ronaldpatino@gmail.com>
2013-07-08 17:14:04 -0500

    Removido html de banner

--- blocks/titulares.php
+++ blocks/titulares.php
@@ -4,15 +4,7 @@
         </div>
         <div class='span4 sidebar'>
 
-            <?php if ( dynamic_sidebar('ultimas_masleidas_portada') ) : else : endif; ?>
-
-            <ul class="thumbnails">
-                <li class="span12">
-                    <div class="thumbnail publicidad">
-                        <img data-src="holder.js/350x350">
-                    </div>
-                </li>
-            </ul>
+        <?php if ( dynamic_sidebar('ultimas_masleidas_portada') ) : else : endif; ?>
 
         </div>
     </div>
