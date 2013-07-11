769b837864992e8b101f8520eba33d0671665de2
ronaldpatino <ronaldpatino@gmail.com>
2013-07-11 17:38:15 -0500

    Eliminado twitter en mobil

--- blocks/twitter.php
+++ blocks/twitter.php
@@ -1,4 +1,4 @@
-    <div class="navbar-twitter navbar-twitter-inverse navbar-twitter-fixed-bottom">
+    <div class="navbar-twitter navbar-twitter-inverse navbar-twitter-fixed-bottom hidden-phone">
             <div class="navbar-twitter-inner">
                 <div class="container nosh">
                     <a href="http://www.twitter.com/mercurioec" target="_blank"><img title="El Mercurio Noticias Ecuador Twitter"  alt="El Mercurio Noticias Ecuador Twitter" class="pull-left img-bottom" src="<?php bloginfo('template_url'); ?>/assets/img/twitter.png"></a>
