d6ac85d5fedfe5d3cd27166d77b8cbf1a714b7aa
ronaldpatino <ronaldpatino@gmail.com>
2013-07-16 12:52:01 -0500

    Agergado sidebar de caricatura

--- blocks/opinion.php
+++ blocks/opinion.php
@@ -3,40 +3,6 @@
     <div class="row-fluid" id='opinion'>
         <div class="span4">
             <?php if ( dynamic_sidebar('caricatura') ) : else : endif; ?>
-
-            <div id="caricaturamain" class="carousel slide">
-                <!-- Carousel items -->
-                <div class="carousel-inner">
-                    <div class="active item">
-                        <ul class="thumbnails sociales-thumbnails">
-                            <li class="span12">
-                                <img alt="" class="caricatura" src="<?php bloginfo('template_url'); ?>/assets/img/1.jpg">
-                                <div class="carousel-caption">
-                                    <p>Fecha.</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="item">
-                        <ul class="thumbnails sociales-thumbnails">
-                            <li class="span12">
-                                <img alt="" class="caricatura" src="<?php bloginfo('template_url'); ?>/assets/img/2.png">
-                                <div class="carousel-caption">
-
-                                    <p>Fecha.</p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-                <!-- Carousel nav -->
-                <a class="left caricatura-carousel-control" href="#caricaturamain" data-slide="prev">&lsaquo;</a>
-                <a class="right caricatura-carousel-control" href="#caricaturamain" data-slide="next">&rsaquo;</a>
-            </div>
-
-
-
         </div>
 
         <div class="span4">
