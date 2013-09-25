9920e46c97d034ffd2a5101567be67fd8c393d75
ronaldpatino <ronaldpatino@gmail.com>
2013-09-25 11:07:46 -0500

    Sacamos noticias en rango de un dia de 00:00 a 24:00 Filtrmos por basura ingresada por el usuario en los posts

--- functions.php
+++ functions.php
@@ -327,7 +327,7 @@ function get_attachment_images($id)
 
 function filter_where($where = '')
 {
-    $where .= " AND post_date <= '" . date('Y-m-d') . "'";
+    $where .= " AND post_date >= '" . date('Y-m-d') . ' 00:00' . "'" . " AND post_date <= '" . date('Y-m-d') . ' 24:00' . "'" ;
     return $where;
 }
 
@@ -902,6 +902,56 @@ function remove_shortcode_page($content) {
 }
 add_filter('the_content', 'remove_shortcode_page');
 
+
+function remove_css($content)
+{
+    $search =
+        array(
+        '@<script[^>]*?>.*?</script>@si',  // Strip out javascript
+        '@<style[^>]*?>.*?</style>@siU',    // Strip style tags properly
+        '@<![\s\S]*?--[ \t\n\r]*>@'         // Strip multi-line comments including CDATA
+    );
+    return preg_replace($search, '', $content);
+}
+add_filter('the_content', 'remove_css');
+
+
+function remove_word_html($text, $allowed_tags = '<strong><p><br>')
+{
+    mb_regex_encoding('UTF-8');
+    //replace MS special characters first
+    $search = array('/&lsquo;/u', '/&rsquo;/u', '/&ldquo;/u', '/&rdquo;/u', '/&mdash;/u');
+    $replace = array('\'', '\'', '"', '"', '-');
+    $text = preg_replace($search, $replace, $text);
+    //make sure _all_ html entities are converted to the plain ascii equivalents - it appears
+    //in some MS headers, some html entities are encoded and some aren't
+    $text = html_entity_decode($text, ENT_QUOTES, 'UTF-8');
+    //try to strip out any C style comments first, since these, embedded in html comments, seem to
+    //prevent strip_tags from removing html comments (MS Word introduced combination)
+    if(mb_stripos($text, '/*') !== FALSE){
+        $text = mb_eregi_replace('#/\*.*?\*/#s', '', $text, 'm');
+    }
+    //introduce a space into any arithmetic expressions that could be caught by strip_tags so that they won't be
+    //'<1' becomes '< 1'(note: somewhat application specific)
+    $text = preg_replace(array('/<([0-9]+)/'), array('< $1'), $text);
+    $text = strip_tags($text, $allowed_tags);
+    //eliminate extraneous whitespace from start and end of line, or anywhere there are two or more spaces, convert it to one
+    $text = preg_replace(array('/^\s\s+/', '/\s\s+$/', '/\s\s+/u'), array('', '', ' '), $text);
+    //strip out inline css and simplify style tags
+    $search = array('#<(strong|b)[^>]*>(.*?)</(strong|b)>#isu', '#<(em|i)[^>]*>(.*?)</(em|i)>#isu', '#<u[^>]*>(.*?)</u>#isu');
+    $replace = array('<b>$2</b>', '<i>$2</i>', '<u>$1</u>');
+    $text = preg_replace($search, $replace, $text);
+    //on some of the ?newer MS Word exports, where you get conditionals of the form 'if gte mso 9', etc., it appears
+    //that whatever is in one of the html comments prevents strip_tags from eradicating the html comment that contains
+    //some MS Style Definitions - this last bit gets rid of any leftover comments */
+    $num_matches = preg_match_all("/\<!--/u", $text, $matches);
+    if($num_matches){
+        $text = preg_replace('/\<!--(.)*--\>/isu', '', $text);
+    }
+    return $text;
+}
+
+add_filter('the_content', 'remove_word_html');
 
 /***
  * Sacamos las imagenes de un post que esten como adjuntas
