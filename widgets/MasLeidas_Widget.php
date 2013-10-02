7a42bf469a788ca653c0f89262dce080f5a87931
ronaldpatino <ronaldpatino@gmail.com>
2013-10-02 11:34:16 -0500

    Mas Leidas arreglado

---
+++ widgets/MasLeidas_Widget.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ba01000660
+ * Date: 02/10/13
+ * Time: 11:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class MasLeidas {
+
+}
