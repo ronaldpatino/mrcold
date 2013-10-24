8f9dfd2de16359092a2972ca411cab34535918dd
ronaldpatino <ronaldpatino@gmail.com>
2013-10-24 10:23:26 -0500

    Agregado timer al carrusel

--- footer.php
+++ footer.php
@@ -1,25 +1,26 @@
-
 <?php wp_footer(); ?>
 <script type="text/javascript">
     jQuery(document).ready(function($) {
-
+        $('.carousel').carousel({
+            interval: 5000
+        });
         /*Clima*/
         $.get("<?php bloginfo('template_url'); ?>/clima.php", function(data) { })
-        .done(function(data) { $("#clima").append(data); })
-        .fail(function() { $("#clima").append(""); });
+            .done(function(data) { $("#clima").append(data); })
+            .fail(function() { $("#clima").append(""); });
         /*Fin clima*/
 
         /*Twitter*/
         $.getJSON('<?php bloginfo('template_url'); ?>/twitter.php', function(data) { })
-        .done(function(data) {  var items = [];
-                                $.each(data, function(key, val) {items.push('<li id="' + key + '">' + val.text + '</li>');});
-                                $('<ul/>', {'id':'ticker','class': 'twitem', html: items.join('')}).appendTo('#twitterlist');
-                                function tick(){
-                                    $('#ticker li:first').slideUp( function () { $(this).appendTo($('#ticker')).slideDown(); });
-                                }
-                                setInterval(function(){ tick () }, 10000);
-        })
-        .fail(function() { });
+            .done(function(data) {  var items = [];
+                $.each(data, function(key, val) {items.push('<li id="' + key + '">' + val.text + '</li>');});
+                $('<ul/>', {'id':'ticker','class': 'twitem', html: items.join('')}).appendTo('#twitterlist');
+                function tick(){
+                    $('#ticker li:first').slideUp( function () { $(this).appendTo($('#ticker')).slideDown(); });
+                }
+                setInterval(function(){ tick () }, 10000);
+            })
+            .fail(function() { });
         /*Fin Twitter*/
 
     });
